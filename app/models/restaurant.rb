75ac7c716323cc689c231ad1c9174689f403f792
Debbie Jong <surrealstardust@gmail.com>
2017-10-11 14:41:52 +0900

    add review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,4 @@
 class Restaurant < ApplicationRecord
-  REST= ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
