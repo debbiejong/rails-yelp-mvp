f46018dad0a4c98c75326a3813a9659c43036a8d
Debbie Jong <surrealstardust@gmail.com>
2017-10-11 14:33:02 +0900

    add restaurants to database and update styling

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-  has many :reviews, dependent: :destroy
+  REST= ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
   # validates :category, presence: true
