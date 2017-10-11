bfda2d1041a4c944bc238c16c0d7158cd69c48c0
Debbie Jong <surrealstardust@gmail.com>
2017-10-11 15:32:19 +0900

    add reviews controllers and update routes and views

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,49 @@
+class ReviewsController < ApplicationController
+
+  # def index
+  #   @reviews = Review.all
+  # end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to review_path(@review)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
